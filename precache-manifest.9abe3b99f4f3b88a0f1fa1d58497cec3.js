ccec1835d0f2f610073ab5cb906a38891dc80927
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-09-07 19:06:42 +0200

    Updates

---
+++ precache-manifest.9abe3b99f4f3b88a0f1fa1d58497cec3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3fd89298848298bf33338c1ca31c6b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9bb80db3fbdb8c3d89c9",
+    "url": "/portfolio/static/css/3.6135517b.chunk.css"
+  },
+  {
+    "revision": "aba336628f48b8d4b4dd",
+    "url": "/portfolio/static/js/2.27aa9a12.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.27aa9a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb80db3fbdb8c3d89c9",
+    "url": "/portfolio/static/js/3.04e8a852.chunk.js"
+  },
+  {
+    "revision": "8eccb215ef0c607271cd",
+    "url": "/portfolio/static/js/4.0e1ce1f6.chunk.js"
+  },
+  {
+    "revision": "739247866c0b35eb1fec",
+    "url": "/portfolio/static/js/5.ce5e55cc.chunk.js"
+  },
+  {
+    "revision": "d7015a302e4d043ee255",
+    "url": "/portfolio/static/js/6.8cfb1c2b.chunk.js"
+  },
+  {
+    "revision": "739546ab29ae11e3e967",
+    "url": "/portfolio/static/js/7.7b753435.chunk.js"
+  },
+  {
+    "revision": "d679c915b4ed04bc68db",
+    "url": "/portfolio/static/js/main.af0703d4.chunk.js"
+  },
+  {
+    "revision": "92abad8d49b9232d6194",
+    "url": "/portfolio/static/js/runtime-main.7703bed3.js"
+  },
+  {
+    "revision": "0e36579e9f08eea7480c832bd8bacb98",
+    "url": "/portfolio/static/media/TomaszKasprowicz.0e36579e.pdf"
+  },
+  {
+    "revision": "7c9e21e38ea403839b1e18747cd399a1",
+    "url": "/portfolio/static/media/clock.7c9e21e3.jpg"
+  },
+  {
+    "revision": "7c85f2c39d12b60c29f4b6c6f3b388d0",
+    "url": "/portfolio/static/media/memoCovered.7c85f2c3.jpg"
+  },
+  {
+    "revision": "9801bcd3905de6acce431a365aff5003",
+    "url": "/portfolio/static/media/memoTouched.9801bcd3.jpg"
+  },
+  {
+    "revision": "b1d29c045b867e30085b7b8b041c8934",
+    "url": "/portfolio/static/media/memoWin.b1d29c04.jpg"
+  },
+  {
+    "revision": "733fa7ad7dec555aa55243d17b591cc4",
+    "url": "/portfolio/static/media/osteriaFront.733fa7ad.jpg"
+  },
+  {
+    "revision": "3068adf8770316dd5fbcc69fbacf7ce8",
+    "url": "/portfolio/static/media/osteriaRecipes.3068adf8.jpg"
+  },
+  {
+    "revision": "7b703abc6e35cdcf9cca90a7e33dcdb1",
+    "url": "/portfolio/static/media/osteriaResponsive.7b703abc.jpg"
+  },
+  {
+    "revision": "c51d32ac0df292cf9aa49df096cb803b",
+    "url": "/portfolio/static/media/weatherAppFront.c51d32ac.jpg"
+  },
+  {
+    "revision": "9f6e83f81695dd3d1569f85fec2860f6",
+    "url": "/portfolio/static/media/weatherAppModal.9f6e83f8.jpg"
+  },
+  {
+    "revision": "29a5f13371ee89fe5a80cb7203c19703",
+    "url": "/portfolio/static/media/weatherAppResponsive.29a5f133.jpg"
+  },
+  {
+    "revision": "2e960d4990bf8887b46cd8c67cc43e85",
+    "url": "/portfolio/static/media/weatherIcon.2e960d49.jpg"
+  }
+]);
