b1b2e2429def1485b12020ae7e1647be74b4d741
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-10-17 16:23:16 +0200

    changed basenmase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render( <Router basename="/portfolio"><App /></Router>,document.getElementById('root'));
+ReactDOM.render( <Router basename="/"><App /></Router>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
